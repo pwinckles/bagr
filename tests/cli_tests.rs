ad9514422a80a792a679520a783b738acdb3a11b
Peter Winckles <pwinckles@pm.me>
2022-02-25 20:56:14 -0600

    another windows attempt

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -1,13 +1,19 @@
-// use std::fs;
-// use std::path::{Path, PathBuf};
+use std::fs;
+use std::path::{Path, PathBuf};
 
 #[test]
 fn bag_cli_tests() {
-    // let base = base_path().join("bag").join("manifest-encoding.in");
-    // write_file(&base.join("dir\\r\\nwith%25everything%0D%0A").join("file.txt"), "complex name\n");
-    // write_file(&base.join("test\\nlf.txt"), "file with lf\n");
-    // write_file(&base.join("test\\rcr.txt"), "file with cr\n");
-    // write_file(&base.join("test%20file.txt"), "file with %\n");
+    let in_base = base_path().join("bag").join("manifest-encoding.in");
+    write_file(&in_base.join("dir\\r\\nwith%25everything%0D%0A").join("file.txt"), "complex name\n");
+    write_file(&in_base.join("test\\nlf.txt"), "file with lf\n");
+    write_file(&in_base.join("test\\rcr.txt"), "file with cr\n");
+    write_file(&in_base.join("test%20file.txt"), "file with %\n");
+
+    let out_base = base_path().join("bag").join("manifest-encoding.out").join("data");
+    write_file(&out_base.join("dir\\r\\nwith%25everything%0D%0A").join("file.txt"), "complex name\n");
+    write_file(&out_base.join("test\\nlf.txt"), "file with lf\n");
+    write_file(&out_base.join("test\\rcr.txt"), "file with cr\n");
+    write_file(&out_base.join("test%20file.txt"), "file with %\n");
 
     trycmd::TestCases::new().case("tests/cmd/bag/*.toml");
 }
@@ -17,18 +23,18 @@ fn rebag_cli_tests() {
     trycmd::TestCases::new().case("tests/cmd/rebag/*.toml");
 }
 
-// fn base_path() -> PathBuf {
-//     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-//     path.push("tests");
-//     path.push("cmd");
-//     path
-// }
-//
-// fn create_dir_all(path: &Path) {
-//     fs::create_dir_all(path).unwrap()
-// }
-//
-// fn write_file(path: &Path, content: &str) {
-//     create_dir_all(path.parent().unwrap());
-//     fs::write(path, content).unwrap();
-// }
+fn base_path() -> PathBuf {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push("tests");
+    path.push("cmd");
+    path
+}
+
+fn create_dir_all(path: &Path) {
+    fs::create_dir_all(path).unwrap()
+}
+
+fn write_file(path: &Path, content: &str) {
+    create_dir_all(path.parent().unwrap());
+    fs::write(path, content).unwrap();
+}
