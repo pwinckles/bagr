4b62b6785d5c2b989ffd00122a561ffc255bcd7d
Peter Winckles <pwinckles@pm.me>
2022-02-25 20:59:01 -0600

    another windows attempt

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -4,15 +4,15 @@ use std::path::{Path, PathBuf};
 #[test]
 fn bag_cli_tests() {
     let in_base = base_path().join("bag").join("manifest-encoding.in");
-    write_file(&in_base.join("dir\\r\\nwith%25everything%0D%0A").join("file.txt"), "complex name\n");
-    write_file(&in_base.join("test\\nlf.txt"), "file with lf\n");
-    write_file(&in_base.join("test\\rcr.txt"), "file with cr\n");
+    write_file(&in_base.join("dir\r\nwith%25everything%0D%0A").join("file.txt"), "complex name\n");
+    write_file(&in_base.join("test\nlf.txt"), "file with lf\n");
+    write_file(&in_base.join("test\rcr.txt"), "file with cr\n");
     write_file(&in_base.join("test%20file.txt"), "file with %\n");
 
     let out_base = base_path().join("bag").join("manifest-encoding.out").join("data");
-    write_file(&out_base.join("dir\\r\\nwith%25everything%0D%0A").join("file.txt"), "complex name\n");
-    write_file(&out_base.join("test\\nlf.txt"), "file with lf\n");
-    write_file(&out_base.join("test\\rcr.txt"), "file with cr\n");
+    write_file(&out_base.join("dir\r\nwith%25everything%0D%0A").join("file.txt"), "complex name\n");
+    write_file(&out_base.join("test\nlf.txt"), "file with lf\n");
+    write_file(&out_base.join("test\rcr.txt"), "file with cr\n");
     write_file(&out_base.join("test%20file.txt"), "file with %\n");
 
     trycmd::TestCases::new().case("tests/cmd/bag/*.toml");
